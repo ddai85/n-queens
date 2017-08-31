653bf86a2fa2cc43ab9b8f0db9fbba49071af882
Hack Reactor Student <student@hackreactor.com>
2017-08-31 11:14:43 -0700

    Fixed single rook solution and multiple solution

--- src/solvers.js
+++ src/solvers.js
@@ -16,24 +16,100 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var rowArray = [];
+  var colArray = [];
+  // create new board
+  var board = new Board({n: n});
+
+  var initializeArray = function(arr) {
+    for (var i = 0; i < n; i++) {
+      arr.push(i);
+    }
+  };
+  
+  var pruneArray = function(arr, val) {
+    arr.splice(arr.indexOf(val), 1);
+  };
+  
+  initializeArray(rowArray);
+  initializeArray(colArray);
+  // toggle first space
+  board.togglePiece(0, 0);
+  pruneArray(rowArray, 0);
+  pruneArray(colArray, 0);
+  // for loop
+  var findNextSpot = function() {
+    // toggle a space inside the loop
+    // check for rook conflict
+    // if conflict
+      // remove toggle
+    // else continue
+      // modify our iteration arrays
+      // break out of current for loops
+      // recurse to start new for loops on a smaller sample set
+    for (var i of rowArray) {
+      for (var j of colArray) {
+        board.togglePiece(i, j);
+        if (board.hasAnyRooksConflicts()) {
+          board.togglePiece(i, j);
+          continue;
+        } else {
+          pruneArray(rowArray, i);
+          pruneArray(colArray, j);
+          return;
+        }
+      }
+    }
+  };
+  
+  while (rowArray.length > 0 && colArray.length > 0) {
+    findNextSpot();
+  }
+  
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return board.rows(); // return array with first solution
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+ //iterate through n positions for row 1 (make internal function) -- pass board state and/or row number??
+  var board = new Board({n: n});
 
+  //debugger;
+  var solutionCount = 0;
+  
+
+  var rookPermutations = function(board, row) {
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      //check to see if base state is reached by checking if row number = n-1
+      if (row === n - 1) {
+        //once at base case, check the board for conflicts-- any board without conflicts adds 1 to solutionCount
+        if (!board.hasAnyRooksConflicts()) {
+          solutionCount++;
+        }
+      } else {
+      
+        var newBoard = new Board(board.rows());
+        //call itself with board state and row number
+        rookPermutations(newBoard, row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+  rookPermutations(board, 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return solutionCount; // returns integer with number of possible solutions
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
-
+  if (n === 0 || n === 2 || n === 3) {
+    return board.rows();  
+  }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
